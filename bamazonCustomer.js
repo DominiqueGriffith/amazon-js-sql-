edecd0339cf79cf1822d71795aed88328c3b9770
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-06-30 16:17:44 -0400

    Changed inquirer to item_id to be connected with data from mySQL.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,7 +25,7 @@ var start = function () {
             readProducts()
         } else {
             connection.end();
-       
+
         }
     })
 
@@ -33,50 +33,94 @@ var start = function () {
 
 function readProducts() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      itemID()
-      
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        itemQuantity()
+
     });
-  }
-
-  function itemID(){
-    inquirer.prompt({
-        name: "wItemID",
-        type: "input",
-        message: "What is the ID of the product they would like to buy?",
-        choices: ["GSH", "GSK","CD","ROS","PMG","PP","DB","GE","MMS","AQH"]
-    }).then(function (answer) {
-        if (answer.wItemID == "GSH" || "GSK" || "CD" ||"ROS" || "PMG" ||"PP" || "DB" ||"GE" || "MMS" || "AQH") {
-            itemQuantity()
-        } else {
-       console.log("Invalid input")
-            connection.end();
-        }
-    })
-
 }
 
-function itemQuantity (){
-    inquirer.prompt({
-        name: "wItemQ",
-        type: "input",
-        message: "How many units of the product would you like to buy?",
-        default: 1
-    }).then(function (answer) {
-        if (answer.wItemQ == 1 || 2 || 3 || 4 || 5) {
-            connection.end();
-        } else {
-       
-            connection.end();
-        }
-    })
+// function itemID() {
+//     inquirer.prompt({
+//         name: "wItemID",
+//         type: "input",
+//         message: "What is the ID of the product they would like to buy?",
+//         choices: ["GSH", "GSK", "CD", "ROS", "PMG", "PP", "DB", "GE", "MMS", "AQH"]
+//     }).then(function (answer) {
+//         if (answer.wItemID == "GSH" || "GSK" || "CD" || "ROS" || "PMG" || "PP" || "DB" || "GE" || "MMS" || "AQH") {
+//             itemQuantity()
+//         } else {
+//             console.log("Invalid input")
+//             connection.end();
+//         }
+//     })
+
+// }
+
+//     }).then(function (answer) {
+//         if (answer.wItemQ == 1 || 2 || 3 || 4 || 5) {
+//             connection.end();
+//         } else {
+
+//             connection.end();
+//         }
+//     })
+
+// }
+
+// How many units of the product would you like to buy?
+
+function itemQuantity() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "rawlist",
+
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].item_id)
+                    }
+                    return choiceArray;
+                },
+                message: "What is the ID of the product they would like to buy?"
+            },
+            {
+                name: "iQuant",
+                type: "rawlist",
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        var quantityC = stock_quantity
+                        choiceArray.push(results[i].stock_quantity)
+                    }
+                    return choiceArray;
+                },
+
+                message: "How many units of the product would you like to buy?"
+            }
+        ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === answer.choice) {
+                        chosenItem = results[i];
+                    }
+
+                    else if (chosenItem.stock_quantity === answer.iQuant) {
+                        console.log(chosenItem.stock_quantity);
+                    }
+
+                }
+
+            });
+
+
+    });
 
 }
-
-function quanityCheck () {
-    
-}
-  
