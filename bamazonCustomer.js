77f35e8c25de672e781fac4c2eff6eddd3243ff9
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-07-02 16:08:34 -0400

    Created functionality to update sql database if the customer chooses an acceptable quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,15 @@ connection.connect(function (err) {
     start();
 })
 
+var pChosenItem;
+var answerIqaunt;
+var qChosenItem;
+var chosenItem;
+var choiceArrayOne = [];
+
+var choiceArrayTwo = [];
+var choiceArrayThree = [];
+
 var start = function () {
     inquirer.prompt({
         name: "showOrNo",
@@ -37,7 +46,7 @@ function readProducts() {
         if (err) throw err;
         // Log all results of the SELECT statement
         console.log(res);
-        itemQuantity()
+        itemCheck()
 
     });
 }
@@ -72,7 +81,7 @@ function readProducts() {
 
 // How many units of the product would you like to buy?
 
-function itemQuantity() {
+function itemCheck() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
 
@@ -82,44 +91,161 @@ function itemQuantity() {
                 type: "rawlist",
 
                 choices: function () {
-                    var choiceArray = [];
+
                     for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].item_id)
+                        choiceArrayOne.push(results[i].item_id)
                     }
-                    return choiceArray;
+                    return choiceArrayOne;
                 },
                 message: "What is the ID of the product they would like to buy?"
             },
             {
                 name: "iQuant",
-                type: "rawlist",
-                choices: function () {
-                    var choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        var quantityC = stock_quantity
-                        choiceArray.push(results[i].stock_quantity)
-                    }
-                    return choiceArray;
-                },
+                type: "input",
+                // choices: function () {
+
+                //     for (var i = 0; i < results.length; i++) {
+                //         // var quantityC = stock_quantity
+                //         choiceArrayTwo.push(results[i].stock_quantity)
+                //         choiceArrayThree.push(results[i].price)
+                //     }
+                //     return choiceArrayTwo;
+                //     return choiceArrayThree;
+                // },
 
                 message: "How many units of the product would you like to buy?"
             }
+            // {
+            //     name: "totalCost",
+            //     type: "input",
+            //     // choices: function () {
+
+            //     //     for (var i = 0; i < results.length; i++) {
+            //     //         // var quantityC = stock_quantity
+            //     //         choiceArrayThree.push(results[i].price)
+
+            //     //     }
+            //     //     return choiceArrayThree;
+            //     // },
+
+            //     message: "Just to confirm, you want to buy " + choiceArrayTwo + "at " + choiceArrayThree + " each?"
+            // }
         ])
             .then(function (answer) {
-                var chosenItem;
+                
                 for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_id === answer.choice) {
-                        chosenItem = results[i];
+                    
+                
+                    if (results[i].item_id === answer.choice && results[i].stock_quantity < answer.iQuant) {
+                        answerIqaunt = answer.iQuant;
+                        chosenItem = results[i].item_id;
+                        // var chosenItemJ = JSON.stringify(chosenItem); 
+                        console.log("You selected  " + chosenItem);
+                        // var qChosenItem = JSON.stringify(results[i]);
+                        qChosenItem = results[i].stock_quantity;
+                        
+                        console.log("Quantity  " + qChosenItem);
+                        // itemQuantity();
+                        console.log("Insufficient quanity. There is only " + qChosenItem + "." + "You selected " + answer.iQuant + ".")
+                        end.connection()
                     }
+                   else if (results[i].item_id === answer.choice && results[i].stock_quantity > answer.iQuant) {
+                    answerIqaunt = answer.iQuant;
 
-                    else if (chosenItem.stock_quantity === answer.iQuant) {
-                        console.log(chosenItem.stock_quantity);
+                        chosenItem = results[i].item_id;
+                        qChosenItem = results[i].stock_quantity;
+                       var newProductQuant = qChosenItem - answerIqaunt;
+                        connection.query(
+                            
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quanity: newProductQuant
+                                },
+                                {
+        
+                                    item_id: chosenItem
+                                }
+                            ],
+                            function(err) {
+                                if (err) throw err;
+                                
+                            }
+                        );
+                        console.log(chosenItem + " products updated!There are(is) " + newProductQuant + " " + chosenItem + " left.");
+                        pChosenItem = results[i].price;
+                        var yourTotal = answer.iQuant * pChosenItem;
+                        console.log("Your total is $" + yourTotal + ".00" + ".");
+                       
+                    
+                        
                     }
+                    else  {
+                        end.connection()
+                    }
+                    // if (results[i].stock_quantity < answer.iQuant) {
+                    //     console.log("Insufficient quanity. There is only " + results[i].stock_quantity + "." + "You selected " + answer + ".")
+                    // }
+                    //     else {
+                    //         console.log("We will process your order. There is " + results[i].stock_quantity)
+                    //     connection.end();
+                    // }
 
                 }
 
             });
+          
 
+       
+            
+                
+            
+
+        //  return qChosenItem
+
+
+
+        // function itemQuantity() {
+
+        //     inquirer.prompt([
+        //         {
+        //             name: "iQuant",
+        //             type: "input",
+        //             // choices: function () {
+
+        //             //     for (var i = 0; i < results.length; i++) {
+        //             //         // var quantityC = stock_quantity
+        //             //         choiceArrayTwo.push(results[i].stock_quantity)
+        //             //     }
+        //             //     return choiceArrayTwo;
+        //             // },
+
+        //             message: "How many units of the product would you like to buy?"
+        //         }
+        //     ])
+        //         .then(function (answer) {
+        //             var chosenItem;
+        //             for (var i = 0; i < results.length; i++) {
+        //                 if (results[i].stock_quantity < answer) {
+        //                     chosenItem = results[i].stock_quantity;
+        //                     // var chosenItemJ = JSON.stringify(chosenItem); 
+        //                     console.log("Isufficient quanity")
+        //                     console.log("You selected  " + chosenItem);
+        //                     // var qChosenItem = JSON.stringify(results[i]);
+        //                     //  qChosenItem = results[i].stock_quantity;
+        //                     // console.log("Quantiy  " + qChosenItem);
+        //                     // itemQuantity();
+
+        //                 }
+        //                 else {
+        //                     connection.end();
+        //                 }
+
+
+        //             }
+
+        //         });
+        // }
 
     });
 
